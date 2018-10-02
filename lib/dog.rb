2b3c95327d89dd6e106c52f28e0bac6b7a1fba01
David masterson <david73180@gmail.com>
2018-10-02 01:12:18 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,16 @@
 class Dog
-   def intialize (name)
-     @name = name
-   end
 
-   def name= (dogs_name)
-     @name = dogs_name
-   end
+def initialize(name)
+  @name = name
+end
 
-   def name
-     @name
-   end
+def name= (dog_name)
+  @name = dog_name
+end
+
+def name
+  @name
+end
+
 
 end
